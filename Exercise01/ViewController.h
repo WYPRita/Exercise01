1947e010b42b85887705e28a649ec7786528f10a
wxweven <wxweven@gmail.com>
2016-04-22 22:45:35 +0800

    Initial Commit

---
+++ Exercise01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercise01
+//
+//  Created by wxweven on 16/4/22.
+//  Copyright © 2016年 wxweven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Exercise01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exercise01
+//
+//  Created by wxweven on 16/4/22.
+//  Copyright © 2016年 wxweven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
